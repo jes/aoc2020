a96176a5596397386b24b36e3f58effc81b1a340
James Stanley <james@incoherency.co.uk>
2020-12-15 05:12:58 +0000

    15

---
+++ 15b.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use List::Util qw(min max);
+
+chomp(my @nums = split /,/, <>);
+
+my %said;
+
+$said{$nums[$_]} = [0,$_,1] for (0..$#nums);
+
+for my $i (@nums..30000000) {
+    my $last = $nums[$#nums];
+    my ($prevspoken, $lastspoken, $timesspoken) = @{ $said{$last} };
+    my $num;
+    if ($timesspoken == 1) {
+        $num = 0;
+    } else {
+        $num = $lastspoken-$prevspoken;
+    }
+
+    ($prevspoken, $lastspoken, $timesspoken) = @{ $said{$num}||[0,0,0] };
+    $said{$num} = [$lastspoken, $i,$timesspoken+1];
+    push @nums, $num;
+}
+
+print $nums[29999999], "\n";
