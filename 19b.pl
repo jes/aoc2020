707f703048fec8808f3572fbb6aa0c837b2f4244
James Stanley <james@incoherency.co.uk>
2020-12-19 06:06:19 +0000

    19b

--- 19b.pl
+++ 19b.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use List::Util qw(all);
+use List::Util qw(min max sum product all);
 
 my $n = 0;
 
@@ -32,19 +32,44 @@ while (<>) {
     }
 }
 
+# 0: 8 11
+# 8: 42 | 42 8 == 42 as many times as you want
+# 11: 42 31 | 42 11 31 == some number of 42 followed by equal number of 31
+# 0 == some number of 42 followed by smaller number of 31
+
 my @chars;
 my $cp = 0;
+my $maxcp;
 
 while (<>) {
     chomp;
     @chars = split //;
     $cp = 0;
-    if (match(0)) {
+    $maxcp = 0;
+    if (bigmatch()) {
         $n++ if $cp == @chars;
+    } else {
     }
 }
 
 print "$n\n";
+
+# check if @chars matches some number of rule 42 followed by some smaller number of rule 31
+sub bigmatch {
+    my $orig_cp = $cp;
+    LOOP:
+    for my $n (1 .. @chars) {
+        $cp = $orig_cp;
+        for my $i (1..$n) {
+            next LOOP if !match(42);
+        }
+        for my $j (1..$n-1) {
+            next LOOP if !match(31);
+            return 1 if $cp == @chars;
+        }
+    }
+    return 0;
+}
 
 sub match {
     my ($r) = @_;
@@ -57,7 +82,10 @@ sub match {
         }
     } elsif ($rule{$r}{type} eq 'terminal') {
         return 0 if $cp >= @chars;
-        return 1 if $chars[$cp++] eq $rule{$r}{char};
+        if ($chars[$cp++] eq $rule{$r}{char}) {
+            $maxcp = $cp if $cp > $maxcp;
+            return 1;
+        }
     }
 
     return 0;
